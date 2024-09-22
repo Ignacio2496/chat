0df932bf62baa4381663f23276eeb8781e93987e
unknown <ignacio.mustafha.fl@wpromote.com>
2024-09-22 18:38:20 -0300

    fix: removed comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Chat from "./pages/Chat";
-import NotFound from "./pages/Not-found";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <Routes>
-      {/* Default route "/" leading to Login */}
       <Route path="/" element={<Login />} />
-
-      {/* Protected chat route */}
       <Route path="/chat" element={<Chat />} />
-
-      {/* Catch all route for any other paths */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
