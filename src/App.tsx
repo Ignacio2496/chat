33fe1f8e32be2073fa4a015c08b09698f409d099
unknown <ignacio.mustafha.fl@wpromote.com>
2024-09-23 06:33:56 -0300

    feat: welcome page and login

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Chat from "./pages/Chat";
 import NotFound from "./pages/NotFound";
+import Welcome from "./pages/Welcome";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Welcome />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/chat" element={<Chat />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
